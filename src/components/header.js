84795d2f9786a050e1399e60d603f8f5e20fd65a
Humza Mujib <hmujib@thoriumdigital.com>
2019-08-26 16:14:17 -0400

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,41 @@
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import './header.css'
+
+const Header = ({ siteTitle }) => (
+  <header className={`header`}>
+    <div className={`left link`}>
+      <h1 className={`header-name`}>
+        <Link className={`link-text`} to='/'>
+          HUMZA MUJIB
+        </Link>
+      </h1>
+    </div>
+    <div className={`right link`}>
+      <h1 className={`header-link`}>
+        <Link className={`link-text light`} to='/'>
+          PHOTOS
+        </Link>
+      </h1>
+    </div>
+    <div className={`right link`}>
+      <h1 className={`header-link`}>
+        <Link className={`link-text light`} to='/'>
+          PROJECTS
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string
+}
+
+Header.defaultProps = {
+  siteTitle: ``
+}
+
+export default Header
